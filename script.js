466d3a6b1dddb6f0743ab13ae4fb297f195e2a46
osigbemhe2020 <62507389+osigbemhe2020@users.noreply.github.com>
2025-10-21 15:14:55 +0100

    added form validation to all fields with js

--- script.js
+++ script.js
@@ -25,6 +25,18 @@
                 resultDiv.innerHTML += '<p data-testid="test-contact-error-email" style="color:red;">Please enter a valid email address.</p>';
                 return;
             }
+            if (nameInput.value.trim() === '') {
+                resultDiv.innerHTML += '<p data-testid="test-contact-error-fname" style="color:red;">All fields are required.</p>';
+                return;
+            }
+            if (subjectInput.value.trim() === '') {
+                resultDiv.innerHTML += '<p data-testid="test-contact-error-subject" style="color:red;">All fields are required.</p>';
+                return;
+            }
+            if (messageInput.value.length < 10) {
+                resultDiv.innerHTML += '<p data-testid="test-contact-error-message" style="color:red;">Message must be at least 10 characters long.</p>';
+                return;
+            }
 
             resultDiv.innerHTML = '<p data-testid="test-contact-success" style="color:green;">Your message has been sent successfully!</p>';
             nameInput.value = '';
